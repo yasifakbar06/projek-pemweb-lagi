40b5dae38cacb0fd0afadae20ee7160802df4d08
fahruanf <fachrubiannafi@gmail.com>
2023-06-13 13:08:35 +0700

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AlatApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('/alat')->group(function () {
+        Route::get('/', [AlatApiController::class, 'showAllAlat']);
+        Route::get('/{id}',[AlatApiController::class, 'detail']);
+    });
+
+    Route::prefix('/category')->group(function () {
+        Route::get('/', [AlatApiController::class, 'showAllCategory']);
+    });
+});
+
+Route::get('/kalender-alat', function() {
+    $order = DB::table('orders')
+    ->join('alats', 'alats.id','=','orders.alat_id')
+    ->join('payments','payments.id','=','orders.payment_id')
+    ->where('orders.status', 2)
+    ->where('payments.status', 3)
+    ->get(['nama_alat AS title','starts AS start','ends AS end']);
+    return json_encode($order);
+});
+
+Route::get('/kalender-alat/{id}', function($id) {
+    $order = DB::table('orders')
+    ->join('alats', 'alats.id','=','orders.alat_id')
+    ->join('payments','payments.id','=','orders.payment_id')
+    ->where('alats.id', $id)
+    ->where('orders.status', 2)
+    ->where('payments.status', 3)
+    ->get(['nama_alat AS title','starts AS start','ends AS end']);
+
+    return json_encode($order);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AlatController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ForgetPasswordController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\RentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +24,79 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',[HomeController::class, 'index'])->name('home');
+Route::get('/detail/{id}',[HomeController::class, 'detail'])->name('home.detail');
+Route::post('/login',[AuthController::class, 'authenticate']);
+Route::get('/daftar',[RegisterController::class,'index'])->name('daftar');
+Route::post('/daftar',[RegisterController::class,'store'])->name('register.store');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/forget-password',[ForgetPasswordController::class,'index'])->name('forgetpassword.index');
+Route::post('/forget-password',[ForgetPasswordController::class,'sendResetLink'])->name('forgetpassword.sendlink');
+
+Route::get('/reset/{token}',[ForgetPasswordController::class,'resetPasswordIndex']);
+Route::post('/reset',[ForgetPasswordController::class,'resetPassword'])->name('resetpassword');
+
+Route::middleware(['auth','superuser'])->group(function () {
+    Route::get('/admin/admin-management',[AdminController::class, 'adminmanagement'])->name('superuser.admin');
+
+    // Alat
+    Route::get('/admin/alat/{id?}',[AlatController::class, 'index'])->name('alat.index');
+    Route::get('/admin/alat/{id}/detail',[AlatController::class,'edit'])->name('alat.edit');
+    Route::patch('/admin/alat/{id}/detail',[AlatController::class,'update'])->name('alat.update');
+    Route::delete('/admin/alat/{id}/detail',[AlatController::class,'destroy'])->name('alat.destroy');
+    Route::post('/admin/alat',[AlatController::class, 'store'])->name('alat.store');
+
+    // Kategori
+    Route::get('/admin/kategori',[CategoryController::class,'index'])->name('kategori.index');
+    Route::post('/admin/kategori',[CategoryController::class,'store'])->name('kategori.store');
+    Route::get('/admin/kategori/{id}/edit',[CategoryController::class,'edit'])->name('kategori.edit');
+    Route::patch('/admin/kategori/{id}',[CategoryController::class,'update'])->name('kategori.update');
+    Route::delete('/admin/kategori/{id}',[CategoryController::class,'destroy'])->name('kategori.destroy');
 });
+
+Route::middleware(['auth','admin'])->group(function () {
+    Route::get('/admin', [AdminController::class,'index'])->name('admin.index');
+
+    //Penyewaan
+    Route::get('/admin/penyewaan',[RentController::class, 'index'])->name('penyewaan.index');
+    Route::get('/admin/penyewaan/detail/{id}',[RentController::class, 'detail'])->name('penyewaan.detail');
+    Route::get('/admin/riwayat-reservasi',[RentController::class,'riwayat'])->name('riwayat-reservasi');
+    Route::patch('/acc/{paymentId}',[OrderController::class,'acc'])->name('acc');
+    Route::patch('/admin/selesai/{id}',[OrderController::class,'alatkembali'])->name('selesai');
+    Route::get('/admin/laporan/cetak',[OrderController::class,'cetak'])->name('cetak');
+    Route::delete('/admin/cancel/{id}',[RentController::class,'destroy'])->name('admin.penyewaan.cancel');
+    Route::patch('/accbayar/{id}',[OrderController::class,'accbayar'])->name('accbayar');
+
+    Route::get('/admin/buat-reservasi/{userId}',[AdminController::class,'newOrderIndex'])->name('admin.buatreservasi');
+    Route::post('/admin/buat-reservasi/order/{userId}',[AdminController::class,'createNewOrder'])->name('admin.createorder');
+
+    // Penyewa atau User
+    Route::get('/admin/usermanagement',[AdminController::class,'usermanagement'])->name('admin.user');
+    Route::post('/admin/usermanagement/new',[AdminController::class,'newUser'])->name('user.new');
+    Route::patch('admin/user/promote/{id}',[UserController::class,'promote'])->name('user.promote');
+    Route::patch('admin/user/demote/{id}',[UserController::class,'demote'])->name('user.demote');
+});
+
+Route::middleware('auth')->group(function() {
+    Route::get('/memberarea',[MemberController::class,'index'])->name('member.index');
+    Route::get('/memberarea/kalender', function() {
+        return view('member.kalender');
+    })->name('member.kalender');
+
+    // Carts
+    Route::post('/memberarea/store/{id}/{userId}',[CartController::class,'store'])->name('cart.store');
+    Route::delete('/memberarea/delete/{id}',[CartController::class,'destroy'])->name('cart.destroy');
+
+    // Orders
+    Route::post('/checkout',[OrderController::class,'create'])->name('order.create');
+    Route::get('/reservasi',[OrderController::class,'show'])->name('order.show');
+    Route::get('/reservasi/detail/{id}',[OrderController::class,'detail'])->name('order.detail');
+    Route::patch('/bayar/{id}',[OrderController::class,'bayar'])->name('bayar');
+    Route::delete('/reservasi/cancel/{id}',[OrderController::class,'destroy'])->name('cancel');
+
+    Route::get('/akun/pengaturan',[UserController::class,'edit'])->name('akun.pengaturan');
+    Route::patch('/akun/pengaturan',[UserController::class,'update'])->name('akun.update');
+    Route::patch('/changepass',[UserController::class,'changePassword'])->name('changepassword');
+});
+
+Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
